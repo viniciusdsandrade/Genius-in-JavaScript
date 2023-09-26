c867dbcd194cc0e3cec71ba9ad0df121bdf3e7dc
Tales Vedovatti <talesassis09@gmail.com>
2023-09-26 19:18:39 -0300

    refactor: Adicionando comentarios no arquivo main

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+
+// Iniciando variáveis globais
 const colors = ['red', 'blue', 'yellow', 'green'];
 let computerSequence = [];
 let playerSequence = [];
 let round = 0;
 let playerTurn = false;
 
+// Selecionando os elementos html e atribuindo-os para variáveis
 const roundDisplay = document.querySelector('.contador-de-rounds');
 const buttons = {
     red: document.getElementById('red'),
@@ -13,6 +16,7 @@ const buttons = {
     center: document.getElementById('btn-center'),
 };
 
+//Função que inicializa o jogo e começa um round
 function playRound() {
     computerSequence.push(colors[Math.floor(Math.random() * 4)]);
     showSequence();
@@ -21,6 +25,7 @@ function playRound() {
     playerTurn = true;
 }
 
+//Função que mostra a sequência de botões a ser apertados e caso ja tenha mostrado todos, habilita para começar a rodada do player
 function showSequence() {
     let i = 0;
     const interval = setInterval(() => {
@@ -37,6 +42,7 @@ function showSequence() {
     }, 1000);
 }
 
+//Função que faz a animação de iluminar/colorir/piscar o botão 
 function illuminateColor(color) {
     buttons[color].classList.remove('btn-grey');
     buttons[color].classList.add(`btn-${color}`);
@@ -46,6 +52,8 @@ function illuminateColor(color) {
     }, 1000);
 }
 
+
+//Função que checa uma jogada do jogador, se estiver correta, o jogo processegue, se estiver errada ela termina o jogo
 function checkPlayerInput(color) {
     if (!playerTurn) return;
 
@@ -67,11 +75,13 @@ function checkPlayerInput(color) {
     }
 }
 
+//Função de final de jogo
 function gameOver() {
     alert(`Fim de jogo! Sua pontuação: ${round}`);
     resetGame();
 }
 
+//Função que reseta o jogo para as configurações iniciais
 function resetGame() {
     computerSequence = [];
     playerSequence = [];
@@ -80,14 +90,15 @@ function resetGame() {
     playerTurn = false;
 }
 
+//Eventos que são acionados ao clicar com o mouse em algum dos botões do jogo, unicamente usados durante o jogo
 buttons.red.addEventListener('click', () => checkPlayerInput('red'));
 buttons.blue.addEventListener('click', () => checkPlayerInput('blue'));
 buttons.yellow.addEventListener('click', () => checkPlayerInput('yellow'));
 buttons.green.addEventListener('click', () => checkPlayerInput('green'));
 
+//Evento que é acionado ao clicar em "Genius" no centro da tela para iniciar o jogo
 buttons.center.addEventListener('click', () => {
     if (!computerSequence.length) {
       playRound();
     }
 });
-  
