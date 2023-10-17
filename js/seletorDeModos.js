7f642c7f5cce68ee9ae6c1ab2d406c202712f14d
Felipinho5 <142360153+Felipinho5@users.noreply.github.com>
2023-10-17 12:22:05 -0300

    Início da codificação do seletor de modos

---
+++ js/seletorDeModos.js
@@ -0,0 +1,15 @@
+let infinity = document.getElementById('infinity');
+let pvp = document.getElementById('pvp');
+let emDobro = document.getElementById('em-dobro');
+
+infinity.addEventListener('input', () => {
+    // Código para preparação do modo Infinity aqui
+});
+
+pvp.addEventListener('input', () => {
+    // Código para preparação do modo PVP aqui
+});
+
+emDobro.addEventListener('input', () => {
+    // Código para preparação do modo Em Dobro aqui
+});
